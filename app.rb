2ef0725bd8e20a01af02414ebfa4126c837a43ef
dhismyname <165235387+dhismyname@users.noreply.github.com>
2024-05-15 04:49:17 +0000

    comet

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ get("/") do
   @currencies = @parsed_response.fetch("currencies")
 
   erb(:homepage)
+
 end
 
 get("/:from_currency") do
@@ -32,7 +33,7 @@ end
 
 get("/:from_currency/:to_currency") do
   @from = params.fetch("from_currency")
-  @to = params.fetch("from_currency")
+  @to = params.fetch("to_currency")
 
   @url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}&from=#{@from}&to=#{@to}&amount=1"
 
